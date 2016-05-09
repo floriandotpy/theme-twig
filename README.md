8a9eb1f45cfd0742e71f12f441c4b6ec29906567
Florian <florian@yootheme.com>
2016-05-09 16:02:56 +0200

    initial import

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hello Twig Theme
+
+An example of how to use Twig to create Pagekit themes. Based on the default [Hello theme](https://github.com/pagekit/theme-hello).
---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Configuration
+ */
+return [
+
+    /**
+     * Unique name that identifies your theme.
+     */
+    'name' => 'theme-twig',
+
+    /**
+     * Change default layout to use a Twig template.
+     */
+    'layout' => 'views:template.twig',
+
+    /**
+     * Define menu positions.
+     * Will be listed in the backend so that users
+     * can assign menus to these positions.
+     */
+    'menus' => [
+
+        'main' => 'Main',
+
+    ],
+
+    /**
+     * Define widget positions.
+     * will be listed in the backend so that users
+     * can publish widgets in these positions.
+     */
+    'positions' => [
+
+        'sidebar' => 'Sidebar',
+
+    ],
+
+    /**
+     * Define theme configuration.
+     * This is the theme's default configuration. During run-time, they will be merged with
+     * settings the user has set in the backend. The default configuration can therefore
+     * be overwritten.
+     */
+    'config' => []
+
+];
